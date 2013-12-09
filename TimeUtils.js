43bacadf44f5513cb8dc06e31f15830303a539c3
Gus <springheeljim@gmail.com>
2013-12-09 13:46:57 +0000

    renames variables in TimeUtils.getColonDisplay

--- TimeUtils.js
+++ TimeUtils.js
@@ -1,50 +1,50 @@
 var TimeUtils={
 	getColonDisplay: function(mtime, displayTenths) {
-		var sections=[];
+		var parts=[];
 		var time=Math.floor(mtime/MSEC_PER_SEC);
 		var tenths=Math.floor((mtime%MSEC_PER_SEC)/(MSEC_PER_SEC/10));
 		var remaining=time;
 		var remainder;
-		var n, str;
 		var divisor;
-		var onFirstSection=true;
-		var haveNonZeroSections=false;
+		var onFirstPart=true;
+		var haveNonZeroParts=false;
 		var display;
-		var sectionMultiples=[24, 60, 60, 1]; //days, hours, minutes, seconds (don't divide seconds into anything)
+		var partMultiples=[24, 60, 60, 1];
+		var n, str;
 
-		var minSections=2; //at least 0:12 if there are only 12 seconds left, but not 0:00:12
+		var minParts=2; //at least 0:12 if there are only 12 seconds left, but not 0:00:12
 		var minDigits=2;
 
-		for(var i=0; i<sectionMultiples.length; i++) {
-			divisor=sectionMultiples[i];
+		for(var i=0; i<partMultiples.length; i++) {
+			divisor=partMultiples[i];
 
-			for(var j=i+1; j<sectionMultiples.length; j++) {
-				divisor*=sectionMultiples[j];
+			for(var j=i+1; j<partMultiples.length; j++) {
+				divisor*=partMultiples[j];
 			}
 
 			remainder=remaining%divisor;
 			n=(remaining-remainder)/divisor;
 			str=""+n;
 
-			if(!onFirstSection) {
+			if(!onFirstPart) {
 				while(str.length<minDigits) {
 					str="0"+str;
 				}
 			}
 
-			if(n>0 || haveNonZeroSections || i>=sectionMultiples.length-minSections) {
-				sections.push(str);
-				onFirstSection=false;
+			if(n>0 || haveNonZeroParts || i>=partMultiples.length-minParts) {
+				parts.push(str);
+				onFirstPart=false;
 			}
 
 			remaining=remainder;
 
 			if(n>0) {
-				haveNonZeroSections=true;
+				haveNonZeroParts=true;
 			}
 		}
 
-		display=sections.join(":");
+		display=parts.join(":");
 
 		if(displayTenths) {
 			display+="."+tenths;
