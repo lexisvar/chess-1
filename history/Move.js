fa2beada7c5f88b185084c804e0ed35f434b1736
Gus <springheeljim@gmail.com>
2013-12-30 23:20:38 +0000

    adds methods that delegate to internal Move back to history Move and removes getMove

--- history/Move.js
+++ history/Move.js
@@ -7,12 +7,45 @@ define(function(require) {
 
 		this.itemType=HistoryItem.MOVE;
 		this._move=move;
+		this._isSelected=false;
 	}
 
 	Move.implement(HistoryItem);
 
-	Move.prototype.getMove=function() {
-		return this._move;
+	Move.prototype.select=function() {
+		this._isSelected=true;
+	}
+
+	Move.prototype.deselect=function() {
+		this._isSelected=false;
+	}
+
+	Move.prototype.isSelected=function() {
+		return this._isSelected;
+	}
+
+	Move.prototype.getFullmove=function() {
+		return this._move.getFullmove();
+	}
+
+	Move.prototype.getColour=function() {
+		return this._move.getColour();
+	}
+
+	Move.prototype.getDot=function() {
+		return this._move.getDot();
+	}
+
+	Move.prototype.getLabel=function() {
+		return this._move.getLabel();
+	}
+
+	Move.prototype.getFullLabel=function() {
+		return this._move.getFullLabel();
+	}
+
+	Move.prototype.getResultingFen=function() {
+		return this._move.getResultingFen();
 	}
 
 	return Move;
