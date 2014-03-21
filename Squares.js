7f6542c68d0ff84aee027aac738e15804f9cefb3
Gus <springheeljim@gmail.com>
2014-03-21 21:15:11 +0000

    adds Squares module - a collection of objects representing the squares with methods for retrieving and enumerating the squares.
    Any references to squares should now be referring to these objects.  Equality
    and identity comparison will still work - the constructor is closed off so there
    will be exactly 64 Squares in existence, one for each square of the chessboard.

---
+++ Squares.js
@@ -0,0 +1,61 @@
+define(function(require) {
+	var Colour = require("./Colour");
+	
+	var files = "abcdefgh";
+	var ranks = "12345678";
+	
+	function Square(squareNo, coordsX, coordsY, algebraic) {
+		this.algebraic = algebraic;
+		this.squareNo = squareNo;
+		
+		this.coords = {
+			x: coordsX,
+			y: coordsY
+		};
+		
+		this.file = files.charAt(coordsX);
+		this.rank = ranks.charAt(coordsY);
+	}
+	
+	var squares = [];
+	var squaresByAlgebraic = [];
+	var squaresByCoords = [];
+	
+	var square, squareNo, algebraic;
+	
+	for(var x = 0; x < 8; x++) {
+		squaresByCoords[x] = [];
+		
+		for(var y = 0; y < 8; y++) {
+			squareNo = y * 8 + x;
+			algebraicSquare = files.charAt(x) + ranks.charAt(y);
+			square = new Square(squareNo);
+			
+			squares[squareNo] = square;
+			squaresByCoords[x][y] = square;
+			squaresByAlgebraic[algebraic] = square;
+		}
+	}
+	
+	return {
+		fromRelativeSquareNo: function(squareNo, viewedAs) {
+			return squares[viewedAs === Colour.BLACK ? 63 - squareNo : squareNo];
+		},
+		
+		fromSquareNo: function(squareNo) {
+			return squares[squareNo];
+		},
+		
+		fromAlgebraic: function(algebraic) {
+			return squaresByAlgebraic[algebraic];
+		},
+		
+		fromCoords: function(coords) {
+			return squaresByCoords[coords.x][coords.y];
+		},
+		
+		forEach: function(callback) {
+			squares.forEach(callback);
+		}
+	};
+});
