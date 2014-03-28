b426b13be7b9bcc68158cf93914609d638f37396
Gus <springheeljim@gmail.com>
2014-03-28 22:22:06 +0000

    adds TimingStyle value object module

---
+++ time/TimingStyle.js
@@ -0,0 +1,31 @@
+define(function(require) {
+	/*
+	TODO add time calculation to these?
+	*/
+	
+	return {
+		fischer: {
+			isIncrementStyle: true
+		},
+		
+		fischerAfter: {
+			isIncrementStyle: true
+		},
+		
+		bronsteinDelay: {
+			isIncrementStyle: true
+		},
+		
+		simpleDelay: {
+			isIncrementStyle: true
+		},
+		
+		suddenDeath: {
+			isIncrementStyle: false
+		},
+		
+		noTimer: {
+			isIncrementStyle: false
+		},
+	};
+});
