f1e2a5f24c8ecff689ed8a67d4d6eae786139615
Gus <springheeljim@gmail.com>
2014-03-30 16:18:40 +0100

    changes validPromotion check to use a regex in PieceType

--- PieceType.js
+++ PieceType.js
@@ -16,7 +16,7 @@ define(function(require) {
 		this.fenStrings = {};
 		this.fenStrings[Colour.white] = sanString.toUpperCase();
 		this.fenStrings[Colour.black] = sanString.toLowerCase();
-		this.isValidPromotion = (sanString !== "P" && sanString !== "K");
+		this.isValidPromotion = !!sanString.match(/[NBRQ]/);
 	}
 	
 	PieceType.prototype.toString = function() {
