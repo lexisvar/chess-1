4d5762fe93a2b9684f704e08bc540d8660213bde
Gus <springheeljim@gmail.com>
2014-03-30 03:54:34 +0100

    replaces first test with basic one move test in Game tests

--- tests/Game.js
+++ tests/Game.js
@@ -1,14 +1,23 @@
 define(function(require, exports, module) {
 	var test = require("chai").assert;
 	var Game = require("../Game");
+	var Square = require("../Square");
+	var PieceType = require("../PieceType");
+	var Piece = require("../Piece");
+	var Colour = require("../Colour");
 	
 	console.log("\033[1m" + module.id + "\033[0m");
 	
 	var tests = {
-		"game can be logged to the console":
+		"white can move from d2 to d4 at the beginning of a standard game":
 		
 		function(game) {
-			console.log(game);
+			game.move(Square.fromAlgebraic("d2"), Square.fromAlgebraic("d4"));
+			test.equal(game.getPosition().getPiece(Square.fromAlgebraic("d2")), null);
+			test.equal(game.getPosition().getPiece(Square.fromAlgebraic("d4")), Piece.get(PieceType.pawn, Colour.white));
+			test.equal(game.getPosition().getActiveColour(), Colour.black);
+			test.equal(game.getPosition().getFiftymoveClock(), 0);
+			test.equal(game.getPosition().getEpTarget(), Square.fromAlgebraic("d3"));
 		}
 	};
 	
@@ -22,7 +31,6 @@ define(function(require, exports, module) {
 			passed++;
 		} catch(error) {
 			console.log("\033[0;31mfailed:\033[0m " + description + ": " + error.message);
-			throw error;
 			failed++;
 		}
 	}
