903c6859a9e923a1eab8c064251f5fdfbaa0c2d9
Gus <springheeljim@gmail.com>
2014-04-01 21:05:53 +0100

    starts playerMoved - main time calculation method.  implements setTimeoutTimer and timeout

--- time/Clock.js
+++ time/Clock.js
@@ -6,8 +6,7 @@ define(function(require) {
 		this.TimeOut = new Event(this);
 		
 		this._options = {
-			clockStartHalfmove: 1,
-			clockStartDelay: 0,
+			startingColour: Colour.white,
 			initialTime: 600,
 			increment: 0,
 			delay: 0,
@@ -21,26 +20,49 @@ define(function(require) {
 			this._options[p] = options[p];
 		}
 		
+		this._isRunning = false;
 		this._halfmove = 0;
 		this._timeoutTimer = null;
-		this._startTime= null;
-		this._lastMoveTime = null;
+		this._startOrLastMoveTime = null;
+		this._activeColour = this._options.startingColour;
 		this._timeLeft = {};
 		this._timeLeft[Colour.white] = this._options.initialTime;
 		this._timeLeft[Colour.black] = this._options.initialTime;
 	}
 	
 	Clock.prototype.start = function() {
-		this._startTime = time();
+		this._isRunning = true;
+		this._startOrLastMoveTime = time();
 		this._setTimeoutTimer();
 	}
 	
-	Clock.prototype.playerMoved = function(colour) {
-		
+	Clock.prototype.playerMoved = function() {
+		if(this._isRunning) {
+			var now = time();
+			var thinkingTime = now - this._startOrLastMoveTime;
+			
+			this._startOrLastMoveTime = now;
+			this._activeColour = this._activeColour.opposite;
+			this._setTimeoutTimer();
+		}
 	}
 	
 	Clock.prototype._setTimeoutTimer = function() {
+		if(this._timeoutTimer !== null) {
+			clearTimeout(this._timeoutTimer);
+		}
 		
+		this._timeoutTimer = setTimeout((function() {
+			this._timeout();
+		}).bind(this), this._timeLeft[this._activeColour]);
+	}
+	
+	Clock.prototype._timeout = function() {
+		clearTimeout(this._timeoutTimer);
+		
+		this.TimeOut.fire({
+			colour: this._activeColour
+		});
 	}
 	
 	return Clock;
