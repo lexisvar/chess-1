061d3a01d0d06a62fc5551ba2313a5c920e39118
Gus <springheeljim@gmail.com>
2014-04-01 22:46:04 +0100

    finishes Clock module
    the Clock doesn't tick at regular intervals but keeps an internal timer for when
    the currently active player will timeout, and fires an event when that happens.
    
    To get the most up-to-date reading of how much time a player has, the getTimeLeft
    method can be used.  Pass a colour (optional, defaults to the active player).
    
    When a player moves, call the playerMoved method.
    
    There are no timing styles coded into the Clock, but the options it accepts cover
    all the timing styles in various combinations, e.g.
    
    {
            increment: 5,
            incrementComesFirst: true
    }
    
    for Fischer-style, or
    
    {
            increment: 5,
            cappedIncrement: true
    }
    
    for Bronstein Delay, or
    
    {
            delay: 5
    }
    
    for Simple Delay

--- time/Clock.js
+++ time/Clock.js
@@ -2,15 +2,19 @@ define(function(require) {
 	var time = require("lib/time");
 	var Colour = require("../Colour");
 	
+	var MILLISECONDS_PER_SECOND = 1000;
+	
 	function Clock(options) {
 		this.TimeOut = new Event(this);
 		
 		this._options = {
 			startingColour: Colour.white,
+			startingFullmove: 1,
 			initialTime: 600,
 			increment: 0,
+			incrementComesFirst: false,
+			cappedIncrement: false,
 			delay: 0,
-			unusedDelayBonus: false,
 			isOvertime: false,
 			overtimeFullmove: 40,
 			overtimeBonus: 600
@@ -21,30 +25,83 @@ define(function(require) {
 		}
 		
 		this._isRunning = false;
-		this._halfmove = 0;
+		this._fullmove = this._options.startingFullmove;
 		this._timeoutTimer = null;
 		this._startOrLastMoveTime = null;
 		this._activeColour = this._options.startingColour;
+		
+		var initialTime = this._options.initialTime * MILLISECONDS_PER_SECOND;
+		
 		this._timeLeft = {};
-		this._timeLeft[Colour.white] = this._options.initialTime;
-		this._timeLeft[Colour.black] = this._options.initialTime;
+		this._timeLeft[Colour.white] = initialTime;
+		this._timeLeft[Colour.black] = initialTime;
 	}
 	
 	Clock.prototype.start = function() {
 		this._isRunning = true;
+		
+		if(this._options.incrementComesFirst) {
+			this._timeLeft[this._activeColour] += this._options.increment;
+		}
+		
 		this._startOrLastMoveTime = time();
 		this._setTimeoutTimer();
+	}
+	
+	Clock.prototype.stop = function() {
+		this._isRunning = false;
+		
+		if(this._timeoutTimer !== null) {
+			clearTimeout(this._timeoutTimer);
+		}
 	}
 	
 	Clock.prototype.playerMoved = function() {
 		if(this._isRunning) {
 			var now = time();
+			var timeLeft = this._timeLeft[this._activeColour];
 			var thinkingTime = now - this._startOrLastMoveTime;
+			var timeUsed = Math.max(0, thinkingTime - this._options.delay * MILLISECONDS_PER_SECOND);
+			var increment = this._options.increment * MILLISECONDS_PER_SECOND;
+			
+			timeLeft -= timeUsed;
+			
+			if(this._options.incrementComesFirst) {
+				this._timeLeft[this._activeColour.opposite] += increment;
+			}
+			
+			else {
+				if(this._options.cappedIncrement) {
+					increment = Math.min(increment, timeUsed);
+				}
+				
+				timeLeft += increment;
+			}
+			
+			if(this._options.isOvertime && this._fullmove === this._options.overtimeFullmove) {
+				timeLeft += this._options.overtimeBonus * MILLISECONDS_PER_SECOND;
+			}
+			
+			if(this._activeColour === Colour.black) {
+				this._fullmove++;
+			}
 			
 			this._startOrLastMoveTime = now;
 			this._activeColour = this._activeColour.opposite;
 			this._setTimeoutTimer();
 		}
+	}
+	
+	Clock.prototype.getTimeLeft = function(colour) {
+		colour = colour || this._activeColour;
+		
+		var timeLeft = this._timeLeft[colour];
+		
+		if(colour === this._activeColour) {
+			timeLeft -= time() - this._startOrLastMoveTime;
+		}
+		
+		return timeLeft;
 	}
 	
 	Clock.prototype._setTimeoutTimer = function() {
@@ -60,9 +117,17 @@ define(function(require) {
 	Clock.prototype._timeout = function() {
 		clearTimeout(this._timeoutTimer);
 		
-		this.TimeOut.fire({
-			colour: this._activeColour
-		});
+		var timeLeft = this.getTimeLeft();
+		
+		if(timeLeft <= 0) {
+			this.TimeOut.fire({
+				colour: this._activeColour
+			});
+		}
+		
+		else {
+			this._setTimeoutTimer();
+		}
 	}
 	
 	return Clock;
