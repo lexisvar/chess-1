088b8dc0f9d7aec02ab47fe13f21b1ce53d3ff60
Gus <springheeljim@gmail.com>
2014-04-02 20:09:06 +0100

    deletes old time modules, adds code to new modules with notes on what to do
    also moves the new modules out of the time/ dir

--- Clock.js
+++ Clock.js
@@ -132,3 +132,44 @@ define(function(require) {
 	
 	return Clock;
 });
+
+/*
+TODO merge this in (from ClockTimeDisplay):
+*/
+
+define(function(require) {
+	require("lib/Array.contains");
+	var TimeParser = require("./TimeParser");
+	var TimingStyle = require("./TimingStyle");
+	
+	return {
+		encode: function(timingStyle, initialTime, increment) {
+			var showDefaultUnits = !isIncrementStyle;
+			var str = TimeParser.encode(initialTime, showDefaultUnits, "m");
+	
+			if(timingStyle.isIncrementStyle) {
+				str += "/" + TimeParser.encode(increment, false, "s");
+			}
+	
+			return str;
+		},
+	
+		encodeFull: function(timingStyle, initialTime, increment, isOvertime, overtimeIncrement, overtimeCutoff) {
+			var display = TimingStyle.noTimer.description;
+	
+			if(timingStyle !== TimingStyle.noTimer) {
+				display = this.encode(timingStyle, initialTime, increment);
+	
+				if(timingStyle !== TimingStyle.suddenDeath) {
+					display += " " + timingStyle.description;
+				}
+	
+				if(isOvertime) {
+					display += " + " + TimeParser.encode(overtimeIncrement, true) + " @ " + overtimeCutoff + " moves";
+				}
+			}
+	
+			return display;
+		}
+	};
+});
--- TimePeriod.js
+++ TimePeriod.js
@@ -1,3 +1,66 @@
+/*
+TODO merge these into one class called "TimePeriod"
+
+TimePeriod.fromString <-- TimeParser
+timePeriod.getColonDisplay <-- TimeUtils
+*/
+
+var TimeUtils = {
+	getColonDisplay: function(time, displayTenths) {
+		var parts = [];
+		var time = Math.floor(time / MSEC_PER_SEC);
+		var tenths = Math.floor((time % MSEC_PER_SEC) / (MSEC_PER_SEC / 10));
+		var remaining = time;
+		var remainder;
+		var divisor;
+		var onFirstPart = true;
+		var haveNonZeroParts = false;
+		var display;
+		var partMultiples = [24, 60, 60, 1];
+		var n, str;
+
+		var minParts = 2;  // at least 0:12 if there are only 12 seconds left, but not 0:00:12
+		var minDigits = 2;
+
+		for(var i = 0; i < partMultiples.length; i++) {
+			divisor = partMultiples[i];
+
+			for(var j = i + 1; j < partMultiples.length; j++) {
+				divisor *= partMultiples[j];
+			}
+
+			remainder = remaining % divisor;
+			n = (remaining - remainder) / divisor;
+			str = "" + n;
+
+			if(!onFirstPart) {
+				while(str.length < minDigits) {
+					str = "0" + str;
+				}
+			}
+
+			if(n > 0 || haveNonZeroParts || i >= partMultiples.length - minParts) {
+				parts.push(str);
+				onFirstPart = false;
+			}
+
+			remaining = remainder;
+
+			if(n > 0) {
+				haveNonZeroParts = true;
+			}
+		}
+
+		display = parts.join(":");
+
+		if(displayTenths) {
+			display += "." + tenths;
+		}
+
+		return display;
+	}
+};
+
 var TimeParser=(function() {
 	var numberRe=/\d/;
 	var unitRe=/[ywdhms]/i;
