80dfa9bf8660d70d277f4e51d11872a1d9c89014
Gus <springheeljim@gmail.com>
2014-04-05 12:43:50 +0100

    re-writes parse and encode methods in TimePeriod module

--- TimePeriod.js
+++ TimePeriod.js
@@ -1,62 +1,118 @@
-/*
-TODO merge these into one class called "TimePeriod"
+define(function() {
+	var Tokeniser = require("lib/Tokeniser");
+	
+	var unitMultipliers = {
+		"s": 1,
+		"m": 60,
+		"h": 3600,
+		"d": 86400,
+		"w": 604800,
+		"y": 31449600
+	};
 
-TimePeriod.fromString <-- TimeParser
-timePeriod.getColonDisplay <-- TimeUtils
-*/
-
-var TimeUtils = {
-	getColonDisplay: function(time, displayTenths) {
-		var parts = [];
-		var time = Math.floor(time / MSEC_PER_SEC);
-		var tenths = Math.floor((time % MSEC_PER_SEC) / (MSEC_PER_SEC / 10));
-		var remaining = time;
-		var remainder;
-		var divisor;
-		var onFirstPart = true;
-		var haveNonZeroParts = false;
-		var display;
-		var partMultiples = [24, 60, 60, 1];
-		var n, str;
-
-		var minParts = 2;  // at least 0:12 if there are only 12 seconds left, but not 0:00:12
-		var minDigits = 2;
-
-		for(var i = 0; i < partMultiples.length; i++) {
-			divisor = partMultiples[i];
-
-			for(var j = i + 1; j < partMultiples.length; j++) {
-				divisor *= partMultiples[j];
+	return {
+		parse: function(timeString, defaultUnits) {
+			var timeInSeconds = 0;
+			var unitsRegex = /[smhdwy]/i;
+			var timeRegex = /\d/;
+			var unitValues = {};
+			var tokeniser = new Tokeniser(timeString);
+			var time, units;
+			
+			while(!tokeniser.isEof()) {
+				tokeniser.seek(timeRegex);
+				time = parseInt(tokeniser.getToken(timeRegex) || "0");
+				
+				tokeniser.seek(unitsRegex);
+				units = tokeniser.getToken(unitsRegex).charAt(0).toLowerCase() || defaultUnits;
+				
+				unitValues[units] = time;
 			}
-
-			remainder = remaining % divisor;
-			n = (remaining - remainder) / divisor;
-			str = "" + n;
-
-			if(!onFirstPart) {
-				while(str.length < minDigits) {
-					str = "0" + str;
+			
+			for(var units in unitValues) {
+				if(units in unitMultipliers) {
+					timeInSeconds += unitValues[units] * unitMultipliers[units];
 				}
 			}
+			
+			return timeInSeconds;
+		},
 
-			if(n > 0 || haveNonZeroParts || i >= partMultiples.length - minParts) {
-				parts.push(str);
-				onFirstPart = false;
+		encode: function(time, defaultUnits) {
+			var remaining = time;
+			var divider, quantity;
+
+			return "ywdhms".split("").map(function(units) {
+				var string = "";
+				
+				divider = unitMultipliers[units];
+
+				if(remaining >= divider) {
+					quantity = Math.floor(remaining / divider);
+					remaining = remaining % divider;
+					string += quantity;
+					
+					if(units !== defaultUnits || remaining > 0) {
+						string += units;
+					}
+				}
+				
+				return string;
+			}).join("") || "0";
+		},
+		
+		getColonDisplay: function(time, displayTenths) {
+			var parts = [];
+			var time = Math.floor(time / MSEC_PER_SEC);
+			var tenths = Math.floor((time % MSEC_PER_SEC) / (MSEC_PER_SEC / 10));
+			var remaining = time;
+			var remainder;
+			var divisor;
+			var onFirstPart = true;
+			var haveNonZeroParts = false;
+			var display;
+			var partMultiples = [24, 60, 60, 1];
+			var n, str;
+	
+			var minParts = 2;  // at least 0:12 if there are only 12 seconds left, but not 0:00:12
+			var minDigits = 2;
+	
+			for(var i = 0; i < partMultiples.length; i++) {
+				divisor = partMultiples[i];
+	
+				for(var j = i + 1; j < partMultiples.length; j++) {
+					divisor *= partMultiples[j];
+				}
+	
+				remainder = remaining % divisor;
+				n = (remaining - remainder) / divisor;
+				str = "" + n;
+	
+				if(!onFirstPart) {
+					while(str.length < minDigits) {
+						str = "0" + str;
+					}
+				}
+	
+				if(n > 0 || haveNonZeroParts || i >= partMultiples.length - minParts) {
+					parts.push(str);
+					onFirstPart = false;
+				}
+	
+				remaining = remainder;
+	
+				if(n > 0) {
+					haveNonZeroParts = true;
+				}
 			}
-
-			remaining = remainder;
-
-			if(n > 0) {
-				haveNonZeroParts = true;
+	
+			display = parts.join(":");
+	
+			if(displayTenths) {
+				display += "." + tenths;
 			}
+	
+			return display;
 		}
-
-		display = parts.join(":");
-
-		if(displayTenths) {
-			display += "." + tenths;
-		}
-
-		return display;
-	}
-};
+	};
+});
