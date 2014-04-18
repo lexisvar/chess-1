645b409c58da795b7510bcbf8290c7b40909a364
Gus <springheeljim@gmail.com>
2014-04-18 22:06:14 +0100

    removes GlickoRating and adds Glicko module - ratings are just numbers now

---
+++ Glicko.js
@@ -0,0 +1,18 @@
+define(function(require) {
+	return {
+		INITIAL_RATING: 1500,
+		
+		calculateNewRatings: function(oldRatings, result) {
+			/*
+			Glicko
+			
+			...
+			
+			return {
+				w: 1234,
+				b: 1634
+			};
+			*/
+		}
+	};
+});
