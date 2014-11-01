19347402fe1c54178983c20c8fc93e1df613fff0
Gus <gus@hogg-blake.co.uk>
2014-11-01 20:33:06 +0000

    corrects references to from.coords.x in Position.getReachableSquares

--- Position.js
+++ Position.js
@@ -417,8 +417,8 @@ define(function(require) {
 
 				if(from === kingHomeSquare) {
 					squares = squares.concat([
-						Square.byCoords[from.x + 2][kingHomeSquare.y],
-						Square.byCoords[from.x - 2][kingHomeSquare.y]
+						Square.byCoords[from.coords.x + 2][kingHomeSquare.y],
+						Square.byCoords[from.coords.x - 2][kingHomeSquare.y]
 					]);
 				}
 
